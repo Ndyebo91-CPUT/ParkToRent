8b50b7d788d80980d2b95ccc192c2446408f48b2
Ndyebo91-CPUT <210018615@mycput.ac.za>
2025-03-28 23:45:59 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/za/ac/cput/repository/iBookingRepository.java
@@ -0,0 +1,70 @@
+package za.ac.cput.repository;
+
+import za.ac.cput.domain.Booking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class iBookingRepository {
+
+    private static iBookingRepository repository= null;
+    private List<Booking> bookingList;
+
+    private BookingRepository(){
+        bookingList= new ArrayList<Booking>();
+    }
+
+    public static iBookingRepository getInstance(){
+        if(repository == null){
+            repository = new BookingRepository();
+        }
+        return repository;
+    }
+
+
+    @Override
+    public List<Booking> getAll() {
+        return bookingList;
+    }
+
+    @Override
+    public boolean create(Booking booking) {
+        return bookingList.add(booking);
+    }
+
+    @Override
+    public Booking read(Integer bookingId) {
+        for(Booking book : bookingList){
+            if(book.getBookingId()== bookingId){
+                return book;
+            }
+        }
+
+
+        return null;
+    }
+
+    @Override
+    public Booking update(Booking booking) {
+        for(int i = 0 ;i<bookingList.size();i++){
+            Booking book = bookingList.get(i);
+            if(book.getBookingId()==booking.getBookingId()){
+                bookingList.set(i,booking);
+                return  booking;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean delete(Integer bookingId) {
+        Booking book = read(bookingId);
+        if(book!= null){
+            bookingList.remove(book);
+            return true;
+        }
+
+        return false;
+    }
+}
